7cd538c09a25cb7e1d10973700c36074523d4f3b
Bozhidara Georgieva <bojidaraggeorgieva@gmail.com>
2016-01-29 04:05:49 +0200

    Assignment

---
+++ src/javaapplication22/pkg10/pkg15/JavaApplication221015.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication22.pkg10.pkg15;
+
+/**
+ *
+ * @author User
+ */
+public class JavaApplication221015 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame.main(null);
+    }
+    
+}
